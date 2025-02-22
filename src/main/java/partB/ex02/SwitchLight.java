807900a7558c8190c1d643fe49b5337b1dc96850
londyy123 <londynnista1128@gmail.com>
2025-02-22 11:59:03 -0500

    Update SwitchLight.java

--- src/main/java/partB/ex02/SwitchLight.java
+++ src/main/java/partB/ex02/SwitchLight.java
@@ -1,10 +1,25 @@
 package partB.ex02;
-
+import java.util.Scanner;
 public class SwitchLight {
     public static String switchLight(){
         String response = "";
-
+        Scanner in = new Scanner(System.in);
         /* Put your code in between these comments : Top */
+        System.out.println("Enter a color code: ");
+        int color = in.nextInt();
+        switch (color) {
+            case 1:
+                System.out.println("Green");
+                break;
+            case 2:
+                System.out.println("Yellow");
+                break;
+            case 3:
+                System.out.println("Red");
+                break;
+            default:
+                System.out.println("invalid code");
+        }
 
 
         /* Put your code in between these comments : Bottom */
