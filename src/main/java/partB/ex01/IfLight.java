ae222dd11ddd8f222e431eeb520e9c57d98c7a41
londyy123 <londynnista1128@gmail.com>
2025-02-22 11:58:46 -0500

    Update IfLight.java

--- src/main/java/partB/ex01/IfLight.java
+++ src/main/java/partB/ex01/IfLight.java
@@ -1,10 +1,24 @@
 package partB.ex01;
-
+import java.util.Scanner;
 public class IfLight {
     public static String ifLight(){
         String response = "";
-
+        Scanner in = new Scanner(System.in);
         /* Put your code in between these comments : Top */
+        System.out.println("Enter a code: ");
+        int light = in.nextInt();
+
+        if (light == 1) {
+            System.out.println("Next traffic light is green");
+        }
+        if (light == 2) {
+            System.out.println("Next traffic light is yellow");
+        }
+        if (light == 3) {
+            System.out.println("Next traffic light is red");
+        } else  {
+            System.out.println("invalid");
+        }
 
 
 
